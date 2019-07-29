45a59b33836d2539c4194f80b2504e9b405669fc
Владимир Сидоров <svs_13@mail.ru>
2019-07-29 20:48:11 +0300

    feature: отредактирована навигация базового слоя (шаблона)

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'testStudio256',
+    'name' => 'Блог',
     'basePath' => dirname(__DIR__),
     'language' => 'ru-RU',
     'bootstrap' => ['log'],
