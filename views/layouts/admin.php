0ae9e49703f60285c04e53068a18fe847fff3630
Владимир Сидоров <svs_13@mail.ru>
2019-07-29 20:32:34 +0300

    feature: добавлена панель навигации для админ-панели

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -3,11 +3,27 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
+use yii\bootstrap\Nav;
+
 \array_unshift($this->params['breadcrumbs'], ['label' => 'Админ-панель', 'url' => ['/admin/authors']]);
 ?>
 
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
-
-    <?= $content; ?>
+    <div class="row">
+        <div class="col-xs-2">
+            <?= Nav::widget([
+                'options' => ['class' => 'nav nav-tabs nav-pills'],
+                'items' => [
+                    ['label' => 'Авторы', 'url' => ['/admin/authors']],
+                    ['label' => 'Категории', 'url' => ['/admin/categories']],
+                    ['label' => 'Посты', 'url' => ['/admin/posts']],
+                ],
+            ]);
+            ?>
+        </div>
+        <div class="col-xs-10">
+            <?= $content; ?>
+        </div>
+    </div>
 
 <?php $this->endContent(); ?>
