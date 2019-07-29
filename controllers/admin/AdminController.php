78dc30d7a9eaba286c6a56ad6f653dee93665a26
Владимир Сидоров <svs_13@mail.ru>
2019-07-29 16:56:02 +0300

    feature: добавлен контроллер и вьюха админской части

---
+++ controllers/admin/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers\admin;
+
+use yii\web\Controller;
+
+/**
+ * Class AdminController
+ * @package app\controllers\admin
+ */
+class AdminController extends Controller
+{
+    public $layout = 'admin';
+}
---
+++ views/layouts/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+
+<?php $this->beginContent('@app/views/layouts/main.php'); ?>
+
+    <?= $content; ?>
+
+<?php $this->endContent(); ?>
+
