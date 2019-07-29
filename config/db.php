57f9b5c95f8b222fd88876806633837f66369b37
Владимир Сидоров <svs_13@mail.ru>
2019-07-29 15:15:42 +0300

    feature: добавлены конфиги подключения к базе данных

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=test_studio256',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
